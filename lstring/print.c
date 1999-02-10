c8c5086b61c71046435a49b333414b5e9973b21c
Vasilis <vasilis.vlachoudis@cern.ch>
1999-02-10 15:45:16 +0100

    RXANSI support by Generoso Martello

--- lstring/print.c
+++ lstring/print.c
@@ -1,6 +1,9 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/print.c,v 1.1 1998/07/02 17:18:00 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/print.c,v 1.2 1999/02/10 15:45:16 bnv Exp $
  * $Log: print.c,v $
+ * Revision 1.2  1999/02/10 15:45:16  bnv
+ * RXANSI support by Generoso Martello
+ *
  * Revision 1.1  1998/07/02 17:18:00  bnv
  * Initial Version
  *
@@ -17,6 +20,10 @@ Lprint( FILE *f, const PLstr str )
 	char	*c;
 
 	if (str==NULL) {
+#ifdef RXANSI
+		if (f==stdout) cprintf("<NULL>");
+		else
+#endif
 		fprintf(f,"<NULL>");
 		return;
 	}
@@ -25,14 +32,26 @@ Lprint( FILE *f, const PLstr str )
 		case LSTRING_TY:
 			c = LSTR(*str);
 			for (l=0; l<LLEN(*str); l++)
+#ifdef RXANSI
+				if (f==stdout) putch(*c++);
+				else
+#endif
 				fputc(*c++,f);
 			break;
 
 		case LINTEGER_TY:
+#ifdef RXANSI
+			if (f==stdout) cprintf("%ld", LINT(*str));
+			else
+#endif
 			fprintf(f,"%ld", LINT(*str));
 			break;
 
 		case LREAL_TY:
+#ifdef RXANSI
+			if (f==stdout) cprintf(formatStringToReal, LREAL(*str));
+			else
+#endif
 			fprintf(f, formatStringToReal, LREAL(*str));
 			break;
 	}
--- lstring/write.c
+++ lstring/write.c
@@ -1,6 +1,9 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/write.c,v 1.1 1998/07/02 17:20:58 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/write.c,v 1.2 1999/02/10 15:45:16 bnv Exp $
  * $Log: write.c,v $
+ * Revision 1.2  1999/02/10 15:45:16  bnv
+ * RXANSI support by Generoso Martello
+ *
  * Revision 1.1  1998/07/02 17:20:58  bnv
  * Initial Version
  *
@@ -20,6 +23,10 @@ Lwrite( FILE *f, const PLstr line, const bool newline)
 	c = LSTR(*line);
 	l = LLEN(*line);
 	while (l--)
+#ifdef RXANSI
+		if (f==stdout) putch(*c++);
+		else
+#endif
 		fputc(*c++,f);
 	if (newline)
 		fputc('\n',f);
