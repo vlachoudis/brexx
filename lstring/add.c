952809189ad9ea953f6f1b5b5f3953e285f3b568
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 09:50:50 +0100

    Cleaned up a little

--- lstring/add.c
+++ lstring/add.c
@@ -1,6 +1,9 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/add.c,v 1.2 1999/03/10 16:55:55 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/add.c,v 1.3 1999/11/26 09:50:50 bnv Exp $
  * $Log: add.c,v $
+ * Revision 1.3  1999/11/26 09:50:50  bnv
+ * Cleaned up a little
+ *
  * Revision 1.2  1999/03/10 16:55:55  bnv
  * Corrected, the use of 'ta' and 'tb'
  *
@@ -9,7 +12,6 @@
  *
  */
 
-#include <ctype.h>
 #include <lerror.h>
 #include <lstring.h>
 
@@ -19,41 +21,20 @@ Ladd( const PLstr to, const PLstr A, const PLstr B)
 {
 	int	ta, tb;
 	double	r;
-	char	*eptr;
 
 	if (LTYPE(*A) == LSTRING_TY) {
-/****
 		ta = _Lisnum(A);
 		if (ta==LSTRING_TY) Lerror(ERR_BAD_ARITHMETIC,0);
-		r = strtod(LSTR(*A),NULL);
-/// With this code with have a problem when a string contains NULL char ie
-///      123\00 , then it is treated as normal number
-****/
-		LASCIIZ(*A);
-		r = strtod(LSTR(*A),&eptr);
-		if (*eptr) {
-			for (; isspace(*eptr); eptr++);
-			if (*eptr) Lerror(ERR_BAD_ARITHMETIC,0);
-		}
-		ta = LREAL_TY;
+		r = lLastScannedNumber;
 	} else {
 		ta = LTYPE(*A);
 		r = TOREAL(*A);
 	}
 
 	if (LTYPE(*B) == LSTRING_TY) {
-/****
 		tb = _Lisnum(B);
 		if (tb==LSTRING_TY) Lerror(ERR_BAD_ARITHMETIC,0);
-		r += strtod(LSTR(*B),NULL);
-****/
-		LASCIIZ(*B);
-		r += strtod(LSTR(*B),&eptr);
-		if (*eptr) {
-			for (; isspace(*eptr); eptr++);
-			if (*eptr) Lerror(ERR_BAD_ARITHMETIC,0);
-		}
-		tb = LREAL_TY;
+		r += lLastScannedNumber;
 	} else {
 		tb = LTYPE(*B);
 		r += TOREAL(*B);
@@ -68,53 +49,4 @@ Ladd( const PLstr to, const PLstr A, const PLstr B)
 		LTYPE(*to) = LREAL_TY;
 		LLEN(*to)  = sizeof(double);
 	}
-
-/********
-	if ( (ta == LINTEGER_TY) && (tb == LINTEGER_TY) ) {
-		if (LTYPE(*A) == LINTEGER_TY)
-			i = LINT(*A);
-		else {
-			LASCIIZ(*A);
-			i = atol(LSTR(*A));
-		}
-		if (LTYPE(*B) == LINTEGER_TY)
-			i += LINT(*B);
-		else {
-			LASCIIZ(*B);
-			i += atol(LSTR(*B));
-		}
-		LINT(*to)  = i;
-		LTYPE(*to) = LINTEGER_TY;
-		LLEN(*to)  = sizeof(long);
-	} else {
-		if (LTYPE(*A) == LSTRING_TY) {
-			LASCIIZ(*A);
-			r = strtod( LSTR(*A), NULL );
-		} else
-			r = TOREAL(*A);
-
-		if (LTYPE(*B) == LSTRING_TY) {
-			LASCIIZ(*B);
-			r += strtod( LSTR(*B), NULL );
-		} else
-			r += TOREAL(*B);
-
-		LREAL(*to) = r;
-		LTYPE(*to) = LREAL_TY;
-		LLEN(*to)  = sizeof(double);
-	}
-*****
-	L2NUM(A);
-	L2NUM(B);
-
-	if ( (LTYPE(*A) == LINTEGER_TY) && (LTYPE(*B) == LINTEGER_TY) ) {
-		LINT(*to)  = LINT(*A) + LINT(*B);
-		LTYPE(*to) = LINTEGER_TY;
-		LLEN(*to)  = sizeof(long);
-	} else {
-		LREAL(*to) = TOREAL(*A) + TOREAL(*B);
-		LTYPE(*to) = LREAL_TY;
-		LLEN(*to)  = sizeof(double);
-	}
-*****/
 } /* Ladd */
