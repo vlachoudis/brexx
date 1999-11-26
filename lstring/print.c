59a3e59672009bc74f0f17ee75856257cacc8591
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 12:52:25 +0100

    Added: Windows CE support Changed: the ANSI_PRINTF to keep the gcc compiler happy

--- lstring/print.c
+++ lstring/print.c
@@ -1,6 +1,10 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/print.c,v 1.4 1999/05/26 16:47:42 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/print.c,v 1.5 1999/11/26 12:52:25 bnv Exp $
  * $Log: print.c,v $
+ * Revision 1.5  1999/11/26 12:52:25  bnv
+ * Added: Windows CE support
+ * Changed: the ANSI_PRINTF to keep the gcc compiler happy
+ *
  * Revision 1.4  1999/05/26 16:47:42  bnv
  * Gene corrections in RXCONIO
  *
@@ -15,45 +19,61 @@
  *
  */
 
-#include <stdio.h>
 #include <lstring.h>
 
 #ifdef RXCONIO
-#define ANSI_FPRINTF(fd,fs,ff)		if (fd==stdout) cprintf(fs,ff); else fprintf(fd,fs,ff)
+#	define ANSI_FPRINTF(fd,fs,ff)	if (fd==STDOUT) \
+				cprintf(fs,ff); else fprintf(fd,fs,ff)
+#	define ANSI_FPUTS(fd,fs)	if (fd==STDOUT) \
+				cprintf(fs); else fprintf(fd,fs)
 #else
-#define ANSI_FPRINTF(fd,fs,ff)		fprintf(fd,fs,ff)
+#	define ANSI_FPRINTF(fd,fs,ff)	fprintf(fd,fs,ff)
+#	define ANSI_FPUTS(fd,fs)	fprintf(fd,fs)
 #endif
 
 /* ---------------- Lprint ------------------- */
 void
-Lprint( FILE *f, const PLstr str )
+Lprint( FILEP f, const PLstr str )
 {
 	size_t	l;
 	char	*c;
+#ifdef WIN
+	char	s[20];
+#endif
 
+#ifndef WIN
 	if (str==NULL) {
-		ANSI_FPRINTF(f,"<NULL>",NULL);
+		ANSI_FPUTS(f,"<NULL>");
 		return;
 	}
+#endif
 
 	switch (LTYPE(*str)) {
 		case LSTRING_TY:
 			c = LSTR(*str);
 			for (l=0; l<LLEN(*str); l++)
-#if defined(RXCONIO)
-				if (f==stdout) {
+#ifdef RXCONIO
+				if (f==STDOUT) {
 					putch(*c++);
 				} else
 #endif
-				fputc(*c++,f);
+				FPUTC(*c++,f);
 			break;
 
 		case LINTEGER_TY:
+#ifdef WIN
+			FPUTS(LTOA(LINT(*str),s,10), f);
+#else
 			ANSI_FPRINTF(f,"%ld", LINT(*str));
+#endif
 			break;
 
 		case LREAL_TY:
-			ANSI_FPRINTF(f, formatStringToReal, LREAL(*str));
+#ifdef WIN
+			FPUTS(GCVT(LREAL(*str),lNumericDigits,s), f);
+#else
+			ANSI_FPRINTF(f, lFormatStringToReal, LREAL(*str));
+#endif
 			break;
 	}
 } /* Lprint */
