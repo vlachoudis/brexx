1c63449b3b114bc72d979b1b6a04907351009e15
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 12:52:25 +0100

    Added: Windows CE support

--- lstring/read.c
+++ lstring/read.c
@@ -1,27 +1,30 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/read.c,v 1.1 1998/07/02 17:18:00 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/read.c,v 1.2 1999/11/26 12:52:25 bnv Exp $
  * $Log: read.c,v $
+ * Revision 1.2  1999/11/26 12:52:25  bnv
+ * Added: Windows CE support
+ *
  * Revision 1.1  1998/07/02 17:18:00  bnv
  * Initial Version
  *
  */
 
-#include <stdio.h>
 #include <lstring.h>
 
 /* ---------------- Lread ------------------- */
 void
-Lread( FILE *f, const PLstr line, long size )
+Lread( FILEP f, const PLstr line, long size )
 {
 	long	l;
 	char	*c;
 	int	ci;
 
+	/* We use the fgetc and not the fread to get rid of the 0x0D */
 	if (size>0) {
 		Lfx(line,(size_t)size);
 		c = LSTR(*line);
 		for (l=0; l<size; l++) {
-			ci = fgetc(f);
+			ci = FGETC(f);
 			if (ci==-1)
 				break;
 			*c++ = ci;
@@ -30,7 +33,7 @@ Lread( FILE *f, const PLstr line, long size )
 	if (size==0) {			/* Read a single line */
 		Lfx(line,LREADINCSIZE);
 		l = 0;
-		while ((ci=fgetc(f))!='\n') {
+		while ((ci=FGETC(f))!='\n') {
 			if (ci==EOF) break;
 			c = LSTR(*line) + l;
 			*c = ci;
@@ -38,30 +41,42 @@ Lread( FILE *f, const PLstr line, long size )
 				Lfx(line, (size_t)l+LREADINCSIZE);
 		}
 	} else {			/* Read entire file */
-		l = ftell(f);
-		fseek(f,0L,SEEK_END);
-		size = ftell(f) - l;
-		fseek(f,l,SEEK_SET);
+#ifndef WCE
+		l = FTELL(f);
+		FSEEK(f,0L,SEEK_END);
+		size = FTELL(f) - l + 1;
+		FSEEK(f,l,SEEK_SET);
+#elif defined(__BORLANDC__)
+		l = FTELL(f);
+                size = FSEEK(f,0L,SEEK_END) - l + 1;
+		FSEEK(f,l,SEEK_SET);
+#else
+		size = GetFileSize(f->handle,NULL) - FTELL(f) + 1;
+#endif
 		if (size>0) { 
 			Lfx(line,(size_t)size);
 			c = LSTR(*line);
 			l = 0;
-			while (!feof(f)) {
-				*c++ = fgetc(f);
+			while (1) {
+				int ch = FGETC(f);
+				if (ch==EOF) break;
+				*c++ = ch;
 				l++;
 			}
-			l--;c--;
-			if (*c=='\n') l--;
-		} else {	/* probably STDIN */
+			/*??? if (*c=='\n') l--; // If it is binary then wrong! */
+		}
+#ifndef WCE
+		else {	/* probably STDIN */
 			Lfx(line,LREADINCSIZE);
 			l = 0;
-			while ((ci=fgetc(f))!=EOF) {
+			while ((ci=FGETC(f))!=EOF) {
 				c = LSTR(*line) + l;
 				*c = ci;
 				if (++l >= LMAXLEN(*line))
 					Lfx(line, (size_t)l+LREADINCSIZE);
 			}
 		}
+#endif
 	}
 	LLEN(*line) = l;
 	LTYPE(*line) = LSTRING_TY;
