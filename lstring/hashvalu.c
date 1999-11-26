b9c9b13e8f4735917e0ac5428aea65b4ccdc012b
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 09:56:55 +0100

    Added: Error message fror CE

--- lstring/hashvalu.c
+++ lstring/hashvalu.c
@@ -1,6 +1,9 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/hashvalu.c,v 1.1 1998/07/02 17:18:00 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/hashvalu.c,v 1.2 1999/11/26 09:56:55 bnv Exp $
  * $Log: hashvalu.c,v $
+ * Revision 1.2  1999/11/26 09:56:55  bnv
+ * Added: Error message fror CE
+ *
  * Revision 1.1  1998/07/02 17:18:00  bnv
  * Initial Version
  *
@@ -12,6 +15,9 @@
 word
 Lhashvalue( const PLstr str )
 {
+#ifdef WCE
+#	error "Lhashvalue is not used!"
+#endif
 	word	value = 0;
 	size_t	i,l;
 
@@ -24,7 +30,7 @@ Lhashvalue( const PLstr str )
 	}
 	for (i=0; i<l; i++) {
 		value ^= LSTR(*str)[i];
-#ifdef __BC__
+#ifdef __BORLANDC__
 		value = _rotl(value,1);
 #else
 		value = ((value>>1) | (value<<7)) & 0xFF;
