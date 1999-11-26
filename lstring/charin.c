e444787142f39680901aa0353bfd1cb990774b50
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 09:53:14 +0100

    Changed: To use the new macros.

--- lstring/charin.c
+++ lstring/charin.c
@@ -1,20 +1,22 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/charin.c,v 1.1 1998/07/02 17:17:00 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/charin.c,v 1.2 1999/11/26 09:53:14 bnv Exp $
  * $Log: charin.c,v $
+ * Revision 1.2  1999/11/26 09:53:14  bnv
+ * Changed: To use the new macros.
+ *
  * Revision 1.1  1998/07/02 17:17:00  bnv
  * Initial revision
  *
  */
 
-#include <stdio.h>
 #include <lstring.h>
 
 /* ---------------- Lcharin ------------------- */
 void
-Lcharin( FILE *f, const PLstr line, const long start, const long length )
+Lcharin( FILEP f, const PLstr line, const long start, const long length )
 {
 	if (start>=1)
-		fseek(f,start-1,SEEK_SET);
+		FSEEK(f,start-1,SEEK_SET);
 	if (length<=0) {
 		LZEROSTR(*line);
 		return;
