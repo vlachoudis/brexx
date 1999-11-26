506ae1404bf4f345ee2371dcedc38084cdbd997b
Vasilis <vasilis.vlachoudis@cern.ch>
1999-11-26 08:53:29 +0100

    BNV Input/Output routines for systems with no stdio.h

--- inc/bio.h
+++ inc/bio.h
@@ -1,21 +1,6 @@
 /*
- * $Id: bio.h,v 1.6 2006/01/26 10:28:56 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/inc/RCS/bio.h,v 1.1 1999/11/26 08:53:29 bnv Exp $
  * $Log: bio.h,v $
- * Revision 1.6  2006/01/26 10:28:56  bnv
- * Added: Absolute and relative paths for windows CE
- *
- * Revision 1.5  2002/06/11 12:37:56  bnv
- * Added: CDECL
- *
- * Revision 1.4  2002/06/06 08:22:34  bnv
- * Corrected: Bfseek
- *
- * Revision 1.3  2001/06/25 18:52:04  bnv
- * Header -> Id
- *
- * Revision 1.2  1999/11/29 14:57:42  bnv
- * Changed: Defines only
- *
  * Revision 1.1  1999/11/26 08:53:29  bnv
  * Initial revision
  *
@@ -42,7 +27,7 @@
 
 #define	EOF		(-1)
 
-#ifndef SEEK_SET
+#if !defined(SEEK_SET)
 #	define	SEEK_SET	(FILE_BEGIN)
 #	define	SEEK_CUR	(FILE_CURRENT)
 #	define	SEEK_END	(FILE_END)
@@ -61,27 +46,23 @@ typedef struct {
 	int	mode;
 } BFILE;
 
-BFILE*	__CDECL	Bfopen(const char *filename, const char *mode);
-int	__CDECL Bfclose(BFILE *stream);
-#ifdef __BORLANDC__
+BFILE	*Bfopen(const char *filename, const char *mode);
+int	Bfclose(BFILE *stream);
+#if defined(__BORLANDC__)
 #	define Bfseek(s,o,w)	_llseek(s->handle,o,w)
+#else
+#	define Bfseek(s,o,w)	SetFilePointer(s->handle,o,0,w)
 #endif
-int	__CDECL Bfseek(BFILE *stream, int distance, int method);
 #define	Bftell(s)	Bfseek(s,0L,SEEK_CUR)
-int	__CDECL Bfeof(BFILE *stream);
-int	__CDECL Bfflush(BFILE *stream);
-void	__CDECL Bfputs(const char *s, BFILE *stream);
-int	__CDECL Bfgetc(BFILE *stream);
-int	__CDECL Bfputc(char ch, BFILE *stream);
+int	Bfeof(BFILE *stream);
+int	Bfflush(BFILE *stream);
+void	Bfputs(const char *s, BFILE *stream);
+int	Bfgetc(BFILE *stream);
+int	Bfputc(char ch, BFILE *stream);
 
-char	__CDECL Bgetchar(void);
-void	__CDECL Bputs(const char *s);
-void	__CDECL Bputch(char ch);
-void	__CDECL Bputint(long num, int length, int radix);
-
-char*	__CDECL Bgetcwd(char* buffer, int maxlen);
-int	__CDECL Bchdir(char* newdir);
-
-void	__CDECL Brel2absdir(LPTSTR buffer, int maxlen, LPCTSTR reldir);
+char	Bgetchar(void);
+void	Bputs(const char *s);
+void	Bputch(char ch);
+void	Bputint(long num, int length, int radix);
 
 #endif
