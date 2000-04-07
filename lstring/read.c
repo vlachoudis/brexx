9d25517edf358f14c40aca389cdf541c5820329b
Vasilis <vasilis.vlachoudis@cern.ch>
2000-04-07 07:11:01 +0200

    Corrected: When trying to read a complete file from STDIN, it was reporting size=1, where it should be negative (unknown)

--- lstring/read.c
+++ lstring/read.c
@@ -1,6 +1,10 @@
 /*
- * $Header: /home/bnv/tmp/brexx/lstring/RCS/read.c,v 1.2 1999/11/26 12:52:25 bnv Exp $
+ * $Header: /home/bnv/tmp/brexx/lstring/RCS/read.c,v 1.3 2000/04/07 07:11:01 bnv Exp $
  * $Log: read.c,v $
+ * Revision 1.3  2000/04/07 07:11:01  bnv
+ * Corrected: When trying to read a complete file from STDIN, it was reporting
+ * size=1, where it should be negative (unknown)
+ *
  * Revision 1.2  1999/11/26 12:52:25  bnv
  * Added: Windows CE support
  *
@@ -43,9 +47,11 @@ Lread( FILEP f, const PLstr line, long size )
 	} else {			/* Read entire file */
 #ifndef WCE
 		l = FTELL(f);
-		FSEEK(f,0L,SEEK_END);
-		size = FTELL(f) - l + 1;
-		FSEEK(f,l,SEEK_SET);
+		if (l>=0) {
+			FSEEK(f,0L,SEEK_END);
+			size = FTELL(f) - l + 1;
+			FSEEK(f,l,SEEK_SET);
+		}
 #elif defined(__BORLANDC__)
 		l = FTELL(f);
                 size = FSEEK(f,0L,SEEK_END) - l + 1;
