3686f4a407e62a603e3e4d01409ab9ba92986df9
Vasilis <vasilis.vlachoudis@cern.ch>
2006-01-26 10:28:32 +0100

    Corrected: To compile on both PocketPC and WindowsCE

--- src/winmain.c
+++ src/winmain.c
@@ -1,6 +1,9 @@
 /*
- * $Id: winmain.c,v 1.6 2004/08/16 15:29:30 bnv Exp $
+ * $Id: winmain.c,v 1.7 2006/01/26 10:28:32 bnv Exp $
  * $Log: winmain.c,v $
+ * Revision 1.7  2006/01/26 10:28:32  bnv
+ * Corrected: To compile on both PocketPC and WindowsCE
+ *
  * Revision 1.6  2004/08/16 15:29:30  bnv
  * Changed: Fonts, check for active window
  *
@@ -27,15 +30,19 @@
 #include <rxdefs.h>
 
 #include <windows.h>
+#include <commctrl.h>
+#include <commdlg.h>
 #include <winio.h>
-#include <cefunc.h>
+#include <winfunc.h>
 
 extern	HWND	_crtWindow;
-extern	void	RxCEInitialize(void);
+extern	void	RxWinInitialize(void);
 
-//#define RUNFILE	"\\cecolors.r"
+#define RFILTER	TEXT("BRexx Files (*.r)\0*.r\0All Files (*.*)\0*.*\0")
+TCHAR	fileName[256] = TEXT("");
+//#define RUNFILE	"\\3dplot.r"
 
-/* --------------------- WinMain ---------------------- */
+/* ---- WinMain ---- */
 int WINAPI
 WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPTSTR lpCmdLine, int nCmdShow )
@@ -54,48 +61,60 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	if (err = WInitWinIO(hInstance,hPrevInstance,nCmdShow))
 		return err;
 
-#if !defined(RUNFILE)
-	if (lpCmdLine[0]==0) {
-		WSetTitle("BRexx");
-		WSetColor(0xF1);
-		PUTS("rexx \"<filename>\" <args>...\n");
-		WSetColor(0xF4);
-		PUTS(VERSIONSTR"\n"
-			"Author: "AUTHOR"\n"
-			"Please report any bugs, fatal errors or comments to the\n"
-			"above address.");
-		WExitWinIO();
-		return 0;
-	}
-#endif
-
-	LINITSTR(tmp);
-#ifdef __BORLANDC__
-	Lscpy(&tmp,lpCmdLine);
-#else
-	Lwscpy(&tmp,lpCmdLine);
-#endif
-	LASCIIZ(tmp);
-
 	LINITSTR(args);
 	LINITSTR(file);
 
-#if !defined(RUNFILE)
-	if ((LSTR(tmp)[0] == '\"') || (LSTR(tmp)[0] == '\'')) {
-		DWORD len;
-		const char *ch = STRCHR(LSTR(tmp)+1,LSTR(tmp)[0]);
-		if (ch)
-			len = (DWORD)ch - (DWORD)LSTR(tmp);
-		else
-			len = LLEN(tmp);
-		Lsubstr(&file, &tmp, 2, len-1, ' ');
-		Lsubstr(&args, &tmp, len+3, LREST, ' ');
-	} else {
-		Lword(&file, &tmp, 1);
-		Lsubword(&args, &tmp, 2, LREST);
-	}
-#else
+#ifdef RUNFILE
 	Lscpy(&file,RUNFILE);
+#else
+	if (lpCmdLine[0]==0) {
+		/* Pop up a dialog for a file to run */
+		OPENFILENAME	ofn;
+
+		memset(&ofn,0,sizeof(ofn));
+		ofn.lStructSize = sizeof(ofn);
+		ofn.hInstance = hInstance;
+		ofn.lpstrFile = fileName;
+		ofn.nMaxFile = sizeof(fileName)/sizeof(TCHAR);
+		ofn.nFilterIndex=1;
+		ofn.lpstrInitialDir = TEXT("");
+		ofn.lpstrFilter = RFILTER;
+		ofn.Flags = OFN_FILEMUSTEXIST;
+		ofn.lpstrTitle = TEXT("Run a BRexx script");
+		ofn.lpstrDefExt = TEXT("r");
+		ofn.hwndOwner = _crtWindow;
+		if (!GetOpenFileName(&ofn)) {
+			WSetTitle("BRexx");
+			WSetColor(0xF1);
+			PUTS("rexx \"<filename>\" <args>...\n");
+			WSetColor(0xF4);
+			PUTS(VERSIONSTR"\n"
+				"Author: "AUTHOR"\n"
+				"Please report any bugs, fatal errors or comments to the\n"
+				"above address.");
+			WExitWinIO();
+			return 0;
+		}
+		LWSCPY(&file,fileName);
+	} else {
+		LINITSTR(tmp);
+		LWSCPY(&tmp,lpCmdLine);
+		LASCIIZ(tmp);
+
+		if ((LSTR(tmp)[0] == '\"') || (LSTR(tmp)[0] == '\'')) {
+			DWORD len;
+			const char *ch = STRCHR(LSTR(tmp)+1,LSTR(tmp)[0]);
+			if (ch)
+				len = (DWORD)ch - (DWORD)LSTR(tmp);
+			else
+				len = LLEN(tmp);
+			Lsubstr(&file, &tmp, 2, len-1, ' ');
+			Lsubstr(&args, &tmp, len+3, LREST, ' ');
+		} else {
+			Lword(&file, &tmp, 1);
+			Lsubword(&args, &tmp, 2, LREST);
+		}
+	}
 #endif
 
 	Lstrcpy(&tmp,&args);
@@ -110,7 +129,9 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	/* --- Initialise --- */
 	RxInitialize("brexxce");
-	RxCEInitialize();
+#ifdef WCE
+	RxWinInitialize();
+#endif
 
 	/* --- Run the program --- */
 	RxRun(LSTR(file),NULL,&args,NULL,NULL);
