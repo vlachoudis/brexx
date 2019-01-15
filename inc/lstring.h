2e94d709fa2ec4c53df41583291d3f751933d991
Mike Großmann <mike.grossmann.de@gmail.com>
2019-01-15 10:30:12 +0100

    LREADINCSIZE set to 4096 when using the JCC compiler. Should reduce heap fragmentation and out of memory exceptions

--- inc/lstring.h
+++ inc/lstring.h
@@ -226,7 +226,11 @@ typedef Lstr	*PLstr;
 
 /* --- file options --- */
 #define LSTARTPOS	-1
+#ifdef JCC
+#define LREADINCSIZE	4096
+#else
 #define LREADINCSIZE	32
+#endif
 #define LREADLINE	0
 #define LREADFILE	-1
 
@@ -425,11 +429,11 @@ DECLMATH( tanh );
 	char
 #	ifdef GREEK
 #	   ifdef MSDOS
-		*clower="abcdefghijklmnopqrstuvwxyz���������������������������������",
-		*cUPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZ����������������������������������",
+		*clower="abcdefghijklmnopqrstuvwxyz ¡¢£¤¥¦§¨©«¬­®¯àªáâãåæçéäè",
+		*cUPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZêëìíîïð",
 #	   else
-		*clower="abcdefghijklmnopqrstuvwxyz������������������������������������",
-		*cUPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZ�����������������������ٶ����ڼ��ۿ�",
+		*clower="abcdefghijklmnopqrstuvwxyzáâãäåæçèéêëìíîïðñóôõö÷øùÜÝÞßúÀüýûàþò",
+		*cUPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÓÔÕÖ×ØÙ¶¸¹ºÚÚ¼¾ÛÛ¿Ó",
 #	   endif
 #	else
 		*clower="abcdefghijklmnopqrstuvwxyz",
